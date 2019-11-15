39af28f8a4443c9157e6818f12c12582973cac54
Dan F-M <foreman.mackey@gmail.com>
2019-11-15 16:06:59 -0500

    extension is working

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-
-/**
- * Initialization data for the ganymede_ext extension.
- */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'ganymede_ext',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension ganymede_ext is activated!');
+    window.addEventListener("_ganymede_ext", (event: Event) => {
+      const command = (event as CustomEvent).detail;
+      if (command) {
+        app.commands.execute(command).catch(() => { console.log("Running: '" + command + "' failed") });
+      }
+    }, false);
   }
 };
 
